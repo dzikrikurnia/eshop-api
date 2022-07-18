39abe364babba41bbad54b47406da90b198f3de1
Dzikri Kurnia <dzikrikurniadeka@gmail.com>
2022-07-18 21:49:48 +0700

    Simple Backend

--- src/server.js
+++ src/server.js
@@ -1,29 +1,48 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const authentication = require('./api/authentication');
+const Database = require('./conf/Database');
+const AuthenticationService = require('./services/mysql/AuthenticationService');
+const AuthenticationValidator = require('./validator/authentication');
 
 const init = async () => {
+  const database = new Database();
+  const authenticationService = new AuthenticationService(database);
+  
 
     const server = Hapi.server({
-        host: process.env.HOST,
-        port: process.env.PORT,
-    routes: {
-      cors: {
-        origin: ['*'],
+      host: process.env.HOST,
+      port: process.env.PORT,
+      routes: {
+        cors: {
+          origin: ['*'],
+        },
       },
-    },
-  });
+    });
+  
 
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: () => ({
-      name: 'DEKA Ganteng',
-    }),
-  });
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: () => ({
+          name: 'Sany Santiastry',
+        }),
+    });
 
-  await server.start();
-  console.log(`Server running at ${server.info.uri}`);
+    //defines internal plugins
+    await server.register([
+      {
+        plugin: authentication,
+        options: {
+          service: authenticationService,
+          validator: AuthenticationValidator,
+        },
+      }
+    ]);
 
-};
+    await server.start();
+    console.log(`Server running at ${server.info.uri}`);
+    
+  };
 
-init();
+  init();
